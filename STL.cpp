092a6d3363a37d8d59d9b9fb991a0de96e473c5b
agraharisaumya <112384231+agraharisaumya@users.noreply.github.com>
2023-06-12 23:11:04 +0530

    Add files via upload

---
+++ STL.cpp
@@ -0,0 +1,36 @@
+//STL-Standard Template Library
+/*C++ STL
+1.Algorithms
+2.Containers
+1.Containers:
+          #Sequence Containers:
+                              Array,Vector,DEque,List,Forward list
+          #Containers Adaptors:
+		                      Stack,Queue,Priority Queue
+		  #Associative Containers:
+		                      Set, map,Multiset,Multimap
+		 #Unordered Associative:
+		                      Unordered Set,Unordered Map,Unorederd Multiset,Unorederd Multimap
+                           
+2.Algorithms:
+            #Binary Search 
+            #lower/Upper Bound
+            #Min/max
+            #Reverse/Rotate
+            #Sort/Swap
+			
+*/
+#include <iostream>
+#include<array>
+using namespace std;
+int main(){
+	int basic[3]={1,2,3};
+	array<int,4> a={1,2,3,4};
+	int size=a.size();
+	cout<<"Size of Array:"<<a<<endl;
+	
+	
+	
+	
+}
+
